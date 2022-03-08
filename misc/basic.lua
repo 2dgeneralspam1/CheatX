1e1af8d238010c9756b71afa21e4484562936586
garfieldcatto <71535863+2dgeneralspam1@users.noreply.github.com>
2022-03-07 20:22:57 -0500

    Create basic.lua

---
+++ misc/basic.lua
@@ -0,0 +1,4 @@
+if syn then else
+loadstring(game:HttpGet('https://raw.githubusercontent.com/2dgeneralspam1/lua-releases/main/iris-compat.lua'))()
+end
+loadstring(game:HttpGet('https://raw.githubusercontent.com/2dgeneralspam1/CheatX/main/loader.lua'))()
