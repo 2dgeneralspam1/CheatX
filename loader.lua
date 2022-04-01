9c93ca627a0708010767f963754444d64f9c17cf
garfieldcatto <71535863+2dgeneralspam1@users.noreply.github.com>
2022-04-01 15:33:33 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,21 @@
--- More coming soon...
+--[[
 
+ ▄████▄   ██░ ██ ▓█████ ▄▄▄     ▄▄▄█████▓▒██   ██▒
+▒██▀ ▀█  ▓██░ ██▒▓█   ▀▒████▄   ▓  ██▒ ▓▒▒▒ █ █ ▒░
+▒▓█    ▄ ▒██▀▀██░▒███  ▒██  ▀█▄ ▒ ▓██░ ▒░░░  █   ░
+▒▓▓▄ ▄██▒░▓█ ░██ ▒▓█  ▄░██▄▄▄▄██░ ▓██▓ ░  ░ █ █ ▒ 
+▒ ▓███▀ ░░▓█▒░██▓░▒████▒▓█   ▓██▒ ▒██▒ ░ ▒██▒ ▒██▒
+░ ░▒ ▒  ░ ▒ ░░▒░▒░░ ▒░ ░▒▒   ▓▒█░ ▒ ░░   ▒▒ ░ ░▓ ░
+  ░  ▒    ▒ ░▒░ ░ ░ ░  ░ ▒   ▒▒ ░   ░    ░░   ░▒ ░
+░         ░  ░░ ░   ░    ░   ▒    ░       ░    ░  
+░ ░       ░  ░  ░   ░  ░     ░  ░         ░    ░  
+░                                                 
+
+IF YOU PAID FOR THIS PRODUCT, YOU WERE SCAMMED.
+
+DISCORD: https://dsc.gg/garfieldwarez
+]]
+
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/2dgeneralspam1/CheatX/main/versions/unsupported.lua'))()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/2dgeneralspam1/CheatX/main/versions/universal.lua'))()
